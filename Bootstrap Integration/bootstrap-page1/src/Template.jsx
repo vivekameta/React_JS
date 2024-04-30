f1a45cf28fc5e015c20f5cdadfc22937e6806798
vivekameta <vickyameta8@gmail.com>
2024-04-30 18:42:33 +0530

    bootstrap-page1

---
+++ Bootstrap Integration/bootstrap-page1/src/Template.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Nav from './Componants/Nav'
+import Header from './Componants/Header'
+import Section_1 from './Componants/Section_1'
+import Section_2 from './Componants/Section_2'
+import Section_3 from './Componants/Section_3'
+import Footer from './Componants/Footer'
+
+function Template() {
+  return (
+    <>
+    <Nav/>
+    <Header/>
+    <Section_1/>
+    <Section_2/>
+    <Section_3/>
+    <Footer/>
+    
+    </>
+  )
+}
+
+export default Template
