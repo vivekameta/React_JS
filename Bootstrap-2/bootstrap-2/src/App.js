05b59ff6c81ba5eeb9f81d8ed07cf36a2a16dd2d
vivekameta <vickyameta8@gmail.com>
2024-05-01 18:16:04 +0530

    bootstrap-page_2

---
+++ Bootstrap-2/bootstrap-2/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Template from './Template';
+
+function App() {
+  return (
+  <>
+   <Template/>
+  </>
+   
+  );
+}
+
+export default App;
---
+++ Bootstrap-2/bootstrap-2/src/Template.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './Componate/Navbar'
+import Header from './Componate/Header'
+import Sec_1 from './Componate/Sec_1'
+import Sec_2 from './Componate/Sec_2'
+
+import Footer from './Componate/Footer'
+
+function Template() {
+  return (
+   <>
+   <Navbar/>
+   <Header/>
+   <Sec_1/>
+   <Sec_2/>
+
+   <Footer/>
+   </>
+  )
+}
+
+export default Template
