579b67e28d388c7b9a4c088f860aa9d41c6f7363
vivekameta <vickyameta8@gmail.com>
2024-05-14 10:42:25 +0530

    pr

---
+++ Localbox Miner/local-box/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Rec from './Rece';
+
+function App() {
+  return (
+    <div className="App">
+<Rec/>
+    </div>
+  );
+}
+
+export default App;
