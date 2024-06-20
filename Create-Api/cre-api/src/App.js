732c2117af85014db4f304944d9e3c019137ff82
vivekameta <vickyameta8@gmail.com>
2024-06-20 17:22:42 +0530

    cre-api

---
+++ Create-Api/cre-api/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PostingData from './PostingData';
+
+function App() {
+  return (
+    <div className="App">
+      <PostingData/>
+    </div>
+  );
+}
+
+export default App;
