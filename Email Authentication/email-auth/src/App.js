b30d74a9691ea4b36d53f0ecb6459f9c42eee87d
vivekameta <vickyameta8@gmail.com>
2024-07-01 17:44:48 +0530

    g-auth PR

---
+++ Email Authentication/email-auth/src/App.js
@@ -0,0 +1,16 @@
+// App.js
+import React from 'react';
+import SignUp from './Signup';
+
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{textAlign:"center",color:"white",paddingTop:"150px"}}>Firebase Authentication</h1> <br />
+      <SignUp/>
+      
+    </div>
+  );
+};
+
+export default App;
